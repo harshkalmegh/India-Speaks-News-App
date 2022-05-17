c77566435c0dcf39a8fa1e81fbff1abb58678dfb
Harsh Kalmegh <87758104+harshkalmegh@users.noreply.github.com>
2022-05-17 12:50:32 +0530

    Update NewsItem.js

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -25,7 +25,7 @@ function NewsItem(props) {
         ) : (
           <LinkPreview
             url={newsUrl}
-            width="335px"
+            width="347px"
             showPlaceholderIfNoImage={true}
           />
         )}
