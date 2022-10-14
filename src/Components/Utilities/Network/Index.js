aa1ebba0aec44a879ef89dfaf66fbd10308e7a37
Harsh Kalmegh <87758104+harshkalmegh@users.noreply.github.com>
2022-10-14 11:51:54 +0530

    Update Index.js

--- src/Components/Utilities/Network/Index.js
+++ src/Components/Utilities/Network/Index.js
@@ -1,11 +1,8 @@
-import axios from "axios";
-export const GetRequest = async (url) => {
+import axios from 'axios';
+export const GetRequest = async url => {
   try {
     const apiResponse = await axios.get(url);
-    // console.log({ apiResponse });
-
     const response = apiResponse.status === 200 ? apiResponse.data : {};
-
     return response;
   } catch (err) {
     return;
