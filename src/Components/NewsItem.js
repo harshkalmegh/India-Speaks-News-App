e1d04f6f0355c21048e0c1ee708ca0141a7f73e3
Harsh Kalmegh <87758104+harshkalmegh@users.noreply.github.com>
2022-10-14 11:51:04 +0530

    Update NewsItem.js

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,21 +1,21 @@
 // @ts-check
 
-import React from "react";
-import { useLinkPreview } from "get-link-preview";
+import React from 'react';
+import {useLinkPreview} from 'get-link-preview';
 
 function NewsItem(props) {
-  let { title, description, imageUrl, newsUrl, author, date, source } = props;
-  
-   const { data } = useLinkPreview(newsUrl);
+  let {title, description, imageUrl, newsUrl, author, date, source} = props;
+
+  const {data} = useLinkPreview(newsUrl);
   return (
     <div className="my-3">
       <div className="card">
         <div
           style={{
-            display: "flex",
-            justifyContent: "flex-end",
-            position: "absolute",
-            right: "0",
+            display: 'flex',
+            justifyContent: 'flex-end',
+            position: 'absolute',
+            right: '0',
           }}
         >
           <span className="badge rounded-pill bg-danger">{source}</span>
@@ -24,22 +24,14 @@ function NewsItem(props) {
           <img src={imageUrl} className="card-img-top" alt="..." />
         ) : (
           // @ts-ignore
-          <img
-            // @ts-ignore
-            src={!data ? "" : data.image}
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={data ? data.image : ''} className="card-img-top" alt="..." />
         )}
-
         <div className="card-body">
           <h1 className="card-title">{title}</h1>
-
           <p className="card-text">{description}...</p>
           <p className="card-text">
             <small className="text-muted">
-              By {!author ? "Anonymous" : author} on{" "}
-              {new Date(date).toLocaleTimeString()}{" "}
+              By {!author ? 'Anonymous' : author} on {new Date(date).toLocaleTimeString()}{' '}
               {new Date(date).toDateString()}
             </small>
           </p>
@@ -48,7 +40,7 @@ function NewsItem(props) {
             href={newsUrl}
             target="_blank"
             className="btn btn-sm btn-dark"
-            style={{ float: "right" }}
+            style={{float: 'right'}}
           >
             Read More
           </a>
