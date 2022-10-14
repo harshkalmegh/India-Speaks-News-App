e98e2310752a8181167f0ee53c0e167e4f789633
Harsh Kalmegh <87758104+harshkalmegh@users.noreply.github.com>
2022-10-14 11:50:40 +0530

    Update News.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react";
-import NewsItem from "./NewsItem";
-import Spinner from "./Spinner";
-import PropTypes from "prop-types";
-import { GetRequest } from "./Utilities/Network/Index";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React, {useState, useEffect, Suspense} from 'react';
+import PropTypes from 'prop-types';
+import {GetRequest} from './Utilities/Network/Index';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+const NewsItem = React.lazy(() => import('./NewsItem'));
+const Spinner = React.lazy(() => import('./Spinner'));
 
 function News(props) {
-  // console.log("News", props);
   const [articles, setArticles] = useState([]);
   const [, setLoading] = useState(true);
   const [page, setPage] = useState(0);
   const [totalResults, setTotalResults] = useState(0);
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const [search, setSearch] = useState(false);
 
-  const capitalizeFirstLetter = (string) => {
+  const capitalizeFirstLetter = string => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
   useEffect(() => {
-    if (input === "") {
+    if (input === '') {
       setArticles([]);
       setSearch(false);
     }
@@ -29,49 +29,49 @@ function News(props) {
     const updateNews = async () => {
       let url;
 
-      if (props.category === "search") {
-        if (input === "") {
+      if (props.category === 'search') {
+        if (input === '') {
           setLoading(false);
           setArticles([]);
         } else {
           url = await GetRequest(
-            `https://newsdata.io/api/1/news?apikey=pub_6879d4cafb3ed0fad900a2dd828177512d21&country=in&language=en,hi&page=${page}&q=${input}`
+            `https://newsdata.io/api/1/news?apikey=pub_6879d4cafb3ed0fad900a2dd828177512d21&country=in&language=en,hi&page=${page}&q=${input}`,
           );
           if (url === undefined) {
             url = await GetRequest(
-              `https://newsdata.io/api/1/news?apikey=pub_68813d1cb70fe729b6aaab9e585c41d6d5d4&country=in&language=en,hi&page=${page}&q=${input}`
+              `https://newsdata.io/api/1/news?apikey=pub_68813d1cb70fe729b6aaab9e585c41d6d5d4&country=in&language=en,hi&page=${page}&q=${input}`,
             );
             if (url === undefined) {
               url = await GetRequest(
-                `https://newsdata.io/api/1/news?apikey=pub_6916b9b2ca77f2af651658ac6664207d137b&country=in&language=en,hi&page=${page}&q=${input}`
+                `https://newsdata.io/api/1/news?apikey=pub_6916b9b2ca77f2af651658ac6664207d137b&country=in&language=en,hi&page=${page}&q=${input}`,
               );
               if (url === undefined) {
                 url = await GetRequest(
-                  `https://newsdata.io/api/1/news?apikey=pub_691773564c6cc88fc0fb0fa52e1f3389a101&country=in&language=en,hi&page=${page}&q=${input}`
+                  `https://newsdata.io/api/1/news?apikey=pub_691773564c6cc88fc0fb0fa52e1f3389a101&country=in&language=en,hi&page=${page}&q=${input}`,
                 );
                 if (url === undefined) {
                   url = await GetRequest(
-                    `https://newsdata.io/api/1/news?apikey=pub_69185e9b582562890079695628ea412ef69c&country=in&language=en,hi&page=${page}&q=${input}`
+                    `https://newsdata.io/api/1/news?apikey=pub_69185e9b582562890079695628ea412ef69c&country=in&language=en,hi&page=${page}&q=${input}`,
                   );
                   if (url === undefined) {
                     url = await GetRequest(
-                      `https://newsdata.io/api/1/news?apikey=pub_69196c0a7d5b2498f5716de4a37b63790a42&country=in&language=en,hi&page=${page}&q=${input}`
+                      `https://newsdata.io/api/1/news?apikey=pub_69196c0a7d5b2498f5716de4a37b63790a42&country=in&language=en,hi&page=${page}&q=${input}`,
                     );
                     if (url === undefined) {
                       url = await GetRequest(
-                        `https://newsdata.io/api/1/news?apikey=pub_69206fe998104e6cd9c141719027e06873f7&country=in&language=en,hi&page=${page}&q=${input}`
+                        `https://newsdata.io/api/1/news?apikey=pub_69206fe998104e6cd9c141719027e06873f7&country=in&language=en,hi&page=${page}&q=${input}`,
                       );
                       if (url === undefined) {
                         url = await GetRequest(
-                          `https://newsdata.io/api/1/news?apikey=pub_69211c566814e0d10a2f82f853254531e9e3&country=in&language=en,hi&page=${page}&q=${input}`
+                          `https://newsdata.io/api/1/news?apikey=pub_69211c566814e0d10a2f82f853254531e9e3&country=in&language=en,hi&page=${page}&q=${input}`,
                         );
                         if (url === undefined) {
                           url = await GetRequest(
-                            `https://newsdata.io/api/1/news?apikey=pub_69226de13f52bf066cf6b2851810bfd207b0&country=in&language=en,hi&page=${page}&q=${input}`
+                            `https://newsdata.io/api/1/news?apikey=pub_69226de13f52bf066cf6b2851810bfd207b0&country=in&language=en,hi&page=${page}&q=${input}`,
                           );
                           if (url === undefined) {
                             url = await GetRequest(
-                              `https://newsdata.io/api/1/news?apikey=pub_69237c4e16b502b115847eae41bd56dd0262&country=in&language=en,hi&page=${page}&q=${input}`
+                              `https://newsdata.io/api/1/news?apikey=pub_69237c4e16b502b115847eae41bd56dd0262&country=in&language=en,hi&page=${page}&q=${input}`,
                             );
                           }
                         }
@@ -89,43 +89,43 @@ function News(props) {
         }
       } else {
         url = await GetRequest(
-          `https://newsdata.io/api/1/news?apikey=pub_6879d4cafb3ed0fad900a2dd828177512d21&country=in&language=en,hi&page=${page}&category=${props.category}`
+          `https://newsdata.io/api/1/news?apikey=pub_6879d4cafb3ed0fad900a2dd828177512d21&country=in&language=en,hi&page=${page}&category=${props.category}`,
         );
         if (url === undefined) {
           url = await GetRequest(
-            `https://newsdata.io/api/1/news?apikey=pub_68813d1cb70fe729b6aaab9e585c41d6d5d4&country=in&language=en,hi&page=${page}&category=${props.category}`
+            `https://newsdata.io/api/1/news?apikey=pub_68813d1cb70fe729b6aaab9e585c41d6d5d4&country=in&language=en,hi&page=${page}&category=${props.category}`,
           );
           if (url === undefined) {
             url = await GetRequest(
-              `https://newsdata.io/api/1/news?apikey=pub_6916b9b2ca77f2af651658ac6664207d137b&country=in&language=en,hi&page=${page}&category=${props.category}`
+              `https://newsdata.io/api/1/news?apikey=pub_6916b9b2ca77f2af651658ac6664207d137b&country=in&language=en,hi&page=${page}&category=${props.category}`,
             );
             if (url === undefined) {
               url = await GetRequest(
-                `https://newsdata.io/api/1/news?apikey=pub_691773564c6cc88fc0fb0fa52e1f3389a101&country=in&language=en,hi&page=${page}&category=${props.category}`
+                `https://newsdata.io/api/1/news?apikey=pub_691773564c6cc88fc0fb0fa52e1f3389a101&country=in&language=en,hi&page=${page}&category=${props.category}`,
               );
               if (url === undefined) {
                 url = await GetRequest(
-                  `https://newsdata.io/api/1/news?apikey=pub_69185e9b582562890079695628ea412ef69c&country=in&language=en,hi&page=${page}&category=${props.category}`
+                  `https://newsdata.io/api/1/news?apikey=pub_69185e9b582562890079695628ea412ef69c&country=in&language=en,hi&page=${page}&category=${props.category}`,
                 );
                 if (url === undefined) {
                   url = await GetRequest(
-                    `https://newsdata.io/api/1/news?apikey=pub_69196c0a7d5b2498f5716de4a37b63790a42&country=in&language=en,hi&page=${page}&category=${props.category}`
+                    `https://newsdata.io/api/1/news?apikey=pub_69196c0a7d5b2498f5716de4a37b63790a42&country=in&language=en,hi&page=${page}&category=${props.category}`,
                   );
                   if (url === undefined) {
                     url = await GetRequest(
-                      `https://newsdata.io/api/1/news?apikey=pub_69206fe998104e6cd9c141719027e06873f7&country=in&language=en,hi&page=${page}&category=${props.category}`
+                      `https://newsdata.io/api/1/news?apikey=pub_69206fe998104e6cd9c141719027e06873f7&country=in&language=en,hi&page=${page}&category=${props.category}`,
                     );
                     if (url === undefined) {
                       url = await GetRequest(
-                        `https://newsdata.io/api/1/news?apikey=pub_69211c566814e0d10a2f82f853254531e9e3&country=in&language=en,hi&page=${page}&category=${props.category}`
+                        `https://newsdata.io/api/1/news?apikey=pub_69211c566814e0d10a2f82f853254531e9e3&country=in&language=en,hi&page=${page}&category=${props.category}`,
                       );
                       if (url === undefined) {
                         url = await GetRequest(
-                          `https://newsdata.io/api/1/news?apikey=pub_69226de13f52bf066cf6b2851810bfd207b0&country=in&language=en,hi&page=${page}&category=${props.category}`
+                          `https://newsdata.io/api/1/news?apikey=pub_69226de13f52bf066cf6b2851810bfd207b0&country=in&language=en,hi&page=${page}&category=${props.category}`,
                         );
                         if (url === undefined) {
                           url = await GetRequest(
-                            `https://newsdata.io/api/1/news?apikey=pub_69237c4e16b502b115847eae41bd56dd0262&country=in&language=en,hi&page=${page}&category=${props.category}`
+                            `https://newsdata.io/api/1/news?apikey=pub_69237c4e16b502b115847eae41bd56dd0262&country=in&language=en,hi&page=${page}&category=${props.category}`,
                           );
                         }
                       }
@@ -155,93 +155,93 @@ function News(props) {
   };
 
   function _handleKeyDown(e) {
-    if (e.key === "Enter") {
-      document.getElementById("myBtnSearch").click();
+    if (e.key === 'Enter') {
+      document.getElementById('myBtnSearch').click();
     }
   }
 
   return (
     <>
-      <h1 className="text-center" style={{ margin: "90px 0 35px 0" }}>
-        {capitalizeFirstLetter(props.category)} Headlines
-      </h1>
-      {/* {loading && <Spinner />} */}
-      <div>
-        {props.category === "search" ? (
-          <>
-            <input
-              type="text"
-              className="SearchNews"
-              placeholder="Search Here"
-              onChange={(e) => {
-                setInput(e.target.value);
-              }}
-              onKeyDown={_handleKeyDown}
-            />
-            <button
-              className="SearchNewsButton"
-              id="myBtnSearch"
-              onClick={() => {
-                setSearch(true);
-              }}
-            >
-              Search
-            </button>
-            <button
-              className="SearchNewsButton"
-              id="myBtnSearch"
-              onClick={() => {
-                setArticles([]);
-                setLoading(false);
-                setInput("");
-              }}
-            >
-              Reset
-            </button>
-          </>
-        ) : (
-          ""
-        )}
-      </div>
-      <InfiniteScroll
-        dataLength={articles.length}
-        next={fetchMoreData}
-        hasMore={articles.length !== totalResults}
-        loader={input.length === 0 ? "" : <Spinner />}
-      >
-        <div className="container">
+      <Suspense fallback={<div>Loading...</div>}>
+        <h1 className="text-center" style={{margin: '90px 0 35px 0'}}>
+          {capitalizeFirstLetter(props.category)} Headlines
+        </h1>
+        {/* {loading && <Spinner />} */}
+        <div>
+          {props.category === 'search' ? (
+            <>
+              <input
+                type="text"
+                className="SearchNews"
+                placeholder="Search Here"
+                onChange={e => {
+                  setInput(e.target.value);
+                }}
+                onKeyDown={_handleKeyDown}
+              />
+              <button
+                className="SearchNewsButton"
+                id="myBtnSearch"
+                onClick={() => {
+                  setSearch(true);
+                }}
+              >
+                Search
+              </button>
+              <button
+                className="SearchNewsButton"
+                id="myBtnSearch"
+                onClick={() => {
+                  setArticles([]);
+                  setLoading(false);
+                  setInput('');
+                }}
+              >
+                Reset
+              </button>
+            </>
+          ) : (
+            ''
+          )}
+        </div>
+        <InfiniteScroll
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length !== totalResults}
+          loader={input.length === 0 ? '' : <Spinner />}
+        >
           <div className="container">
-            <div className="row">
-              {articles.map((element, key) => {
-                return (
-                  <div className="col-md-4" key={key}>
-                    <NewsItem
-                      key={element.link}
-                      title={element.title ? element.title : ""}
-                      description={
-                        element.description ? element.description : ""
-                      }
-                      imageUrl={element.image_url}
-                      newsUrl={element.link}
-                      author={element.creator}
-                      date={`${element.pubDate}z`}
-                      source={element.source_id}
-                    />
-                  </div>
-                );
-              })}
+            <div className="container">
+              <div className="row">
+                {articles.map((element, key) => {
+                  return (
+                    <div className="col-md-4" key={key}>
+                      <NewsItem
+                        key={element.link}
+                        title={element.title ? element.title : ''}
+                        description={element.description ? element.description : ''}
+                        imageUrl={element.image_url}
+                        newsUrl={element.link}
+                        author={element.creator}
+                        date={`${element.pubDate}z`}
+                        source={element.source_id}
+                      />
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
-        </div>
-      </InfiniteScroll>
+        </InfiniteScroll>
+      </Suspense>
     </>
   );
 }
 
 News.defaultProps = {
-  country: "in",
+  country: 'in',
   pageSize: 6,
-  category: "general",
+  category: 'general',
 };
 News.propTypes = {
   country: PropTypes.string,
